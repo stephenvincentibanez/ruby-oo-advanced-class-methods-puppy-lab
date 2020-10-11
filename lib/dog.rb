81818649b1e28cff44cfd12ec4a0b03acb0f6e28
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-10 18:07:27 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+class Dog
+
+@@all = []
+
+def self.all
+    @@all
+end 
+
+def self.clear_all
+    @@all.clear
+end 
+
+def self.print_all
+    @@all.each do |d|
+        puts d.name
+    end 
+end 
+
+attr_accessor :name
+
+def initialize (name)
+    @name = name
+    self.save
+end 
+
+def save
+    @@all << self
+end 
+
+# binding.pry
+end
